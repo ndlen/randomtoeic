5ed99197fe061133092dd311c67b7d9aeed42aa8
Duc Len <nguyenduclenqna@gmail.com>
2025-11-29 15:42:57 +0700

    up

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f87553f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tjcvdu27ah8"
+    "revision": "0.q62uu95k9dg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
